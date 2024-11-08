c04ecd3cb1ae09798b0aca8819aef039b4783552
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-11-08 14:15:02 -0800

    finished

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Hello World!')
+const http = require('http'); 
+
+const hostname = '127.0.0.1'; 
+const port = 3000; 
+
+const server = http.createServer((req, res) => {
+
+  const url = req.url; 
+
+  if (url === "/") {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+  } else if (url === "/about") {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1 style="color: red">Hello There!</h1>');
+    res.write('<p>I wonder what else we can send...</p>');
+    res.end();
+  } else if (url === "/info") {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1 style="color: purple">Hello! This is the info page.</h1>');
+    res.write('<p>This is pretty cool.</p>');
+    res.end();
+  }
+
+});
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
